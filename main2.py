3a8dc2581f92ab4f399b799c8a8197d827b51a67
morganclauss <morgan.lovely@live.com>
2022-08-18 08:45:23 -0400

    fix

--- main2.py
+++ main2.py
@@ -1,11 +1,3 @@
-echo "# Morg" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/morganclauss/Morg.git
-git push -u origin main
-
 
 
 import streamlit as st
